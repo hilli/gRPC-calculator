06e79c1f3f0d995a4dffb8e995dd6baf83902da5
Jens Hilligsøe <jens@hilli.dk>
2021-08-03 14:48:11 +0200

    Take values to be summarised from the command line args

--- calculator/calculator-client/client.go
+++ calculator/calculator-client/client.go
@@ -3,6 +3,8 @@ package main
 import (
 	"context"
 	"log"
+	"os"
+	"strconv"
 
 	pb "github.com/hilli/gRPC-calculator/calculator/calculatorpb"
 	"google.golang.org/grpc"
@@ -24,9 +26,16 @@ func main() {
 
 func doUnary(c pb.CalculatorServiceClient) {
 	log.Printf("Starting Sum request")
+	var first_number, second_number int32 = 2, 3
+	if len(os.Args) > 2 {
+		f64,_ := strconv.ParseInt(os.Args[1], 10, 32)
+		s64,_ := strconv.ParseInt(os.Args[2], 10, 32)
+		first_number = int32(f64)
+		second_number = int32(s64)
+	}
 	req := &pb.SumRequest{
-		FirstNumber: 2,
-		SecondNumber: 3,
+		FirstNumber: first_number,
+		SecondNumber: int32(second_number),
 	}
 	res, err := c.Sum(context.Background(), req)
 	if err != nil {
